d71f91b847daeba1927e5825546015df4083dce4
Aiden Rathsam <rathsamaiden@gmail.com>
2025-10-16 07:30:13 -0400

    calcTotal works on initial addItem and created remove func

--- cartScript.js
+++ cartScript.js
@@ -1,20 +1,46 @@
-let currentPrice;
+let totalPrice
+let priceQuant = new Map()
+priceQuant.set(`Coffee Cup`, [6.99,0])
+priceQuant.set(`T-Shirt`, [19.99,0])
+priceQuant.set(`Album 1`, [12.99,0])
+priceQuant.set(`Album 2`, [14.99,0])
+priceQuant.set(`Album 3`, [9.99,0])
+priceQuant.set(`Album 4`, [19.99,0])
+let entry
 
 function addItem(img, title, price){
-    currentPrice = price
-    let template = document.getElementById('cart-product');
-    let container = document.getElementById('cart-container');
-    let instance = template.content.cloneNode(true);
+    entry = priceQuant.get(title)
+    if (entry[1] !== 0){
+        return
+    }
+    let template = document.getElementById('cart-product')
+    let container = document.getElementById('cart-container')
+    let instance = template.content.cloneNode(true)
     container.appendChild(instance)
     let imgClass = document.querySelectorAll(`.cart-item-image`)
     imgClass[imgClass.length - 1].src = img
     let titleClass = document.querySelectorAll(`.cart-item-title`)
     titleClass[titleClass.length - 1].innerHTML = title
     let priceClass = document.querySelectorAll(`.cart-price`)
-    priceClass[priceClass.length - 1].innerHTML = `$${price}`
-    calcTotal(price)
+    priceClass[priceClass.length - 1].innerHTML = `$${price.toFixed(2)}`
+    priceQuant.set(title, [entry[0], entry[1]+1])
+    calcTotal()
+}
+
+function removeItem(btn){
+    console.log(btn)
+    let parent = btn.parentElement.parentElement
+    parent.remove()
 }
 
 function calcTotal(){
-    
+    totalPrice = 0
+    priceQuant.forEach(val => {
+        totalPrice += val[0]*val[1]
+    })
+    document.getElementById(`final-price`).innerHTML = `$${totalPrice.toFixed(2)}`
+}
+
+function addQuant(){
+
 }
